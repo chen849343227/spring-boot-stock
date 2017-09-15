7864db85b058412eb1dfc562e2ef5fdb25fb45e6
王佳建 <3045293786@qq.com>
2017-09-15 11:42:54 +0800

    新增行情及个人中心模块

---
+++ src/main/resources/static/consult/js/consult_model.js
@@ -0,0 +1,59 @@
+$(function () {
+    $.ajax({
+        url: 'http://192.168.31.68:8080/stock/hkall',
+        type: 'POST',
+        dataType: 'json',
+        timeout: 1000,
+        cache: false,
+        data: {
+            type: 1,
+            page: 1
+        },
+        beforeSend: LoadFunction, //加载执行方法
+        error: erryFunction,  //错误执行方法
+        success: succFunction //成功执行方法
+    });
+    function LoadFunction() {
+        /*$("#hsi-stock").html('加载中...');*/
+    }
+    function erryFunction() {
+        alert("error");
+    }
+    function succFunction(stock) {
+        // $("#list").html('');
+        console.log(stock);
+        console.log(stock.response.result.data);
+        var stocks = stock.response.result.data;
+        $.each(stocks, function() {
+            /*console.log(this.name);
+            console.log(this.eps);*/
+            var number = this.symbol;
+            $('#hsi-stock').append("<tr><td><a onclick='oncli("+this.symbol+")'>"+this.symbol+"</a></td><td>"+this.name+"</td><td class='up'>"+this.buy+"</td><td class='up'>"+this.changepercent+"%</td><td class='up'>"+this.pricechange+"</td><td>"+this.stocks_sum+"</td><td>"+this.amount+"</td><td class='text-center'><a>模拟交易</a></td></tr>");
+            $('#hsi-stock1').append("<tr><td>"+this.symbol+"</td><td>"+this.name+"</td><td>"+this.dividend+"</td><td>"+this.stocks_sum+"</td><td>"+this.eps+"</td><td>"+this.volume+"</td><td>"+this.stocks_sum+"</td><td><a>模拟交易</a><a><span></span>自选</a></td></tr>");
+            $('#hsi-stock2').append("<tr><td>"+this.symbol+"</td><td>"+this.name+"</td><td>"+this.dividend+"</td><td>"+this.stocks_sum+"</td><td>"+this.eps+"</td><td>"+this.volume+"</td><td>"+this.stocks_sum+"</td><td><a>模拟交易</a><a><span></span>自选</a></td></tr>");
+        });
+    }
+});
+
+/*定义oncli函数，请求个股详细信息*/
+function oncli(number) {
+    console.log(number);
+    $(function () {
+        $.ajax({
+            url: '/stock/hk',
+            type: 'POST',
+            dataType: 'json',
+            timeout: 1000,
+            cache: false,
+            data: {
+                num: this.symbol
+            },
+            error: function () {
+            alert("！！！");
+            },
+            success: function (data) {
+            console.log(data);
+            }
+        });
+    });
+};
